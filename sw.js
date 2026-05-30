50c9f2120e098168aed80bac9af7f953648b8d6e
foustbrothersllc <foustbrothersllc@gmail.com>
2026-05-29 22:59:55 -0400

    Update sw.js

--- sw.js
+++ sw.js
@@ -2,7 +2,7 @@
    Driver Call List — sw.js
    ============================================================ */
 const CACHE_VERSION = 'v' + (function() {
-  return '20250517-19';
+  return '20250529-RESTORE';
 })();
 const CACHE_NAME = 'driver-call-list-' + CACHE_VERSION;
 const APP_SHELL = [
