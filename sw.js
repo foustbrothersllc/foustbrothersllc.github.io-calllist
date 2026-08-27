fd0d457e18d7a06eccd8929f92849b409c80cdc6
foustbrothersllc <foustbrothersllc@gmail.com>
2026-08-27 01:39:17 -0400

    Update CACHE_VERSION to new version

--- sw.js
+++ sw.js
@@ -2,7 +2,7 @@
    Driver Call List — sw.js
    ============================================================ */
 const CACHE_VERSION = 'v' + (function() {
-  return '20250517-19';
+  return '20260827-1';
 })();
 const CACHE_NAME = 'driver-call-list-' + CACHE_VERSION;
 const APP_SHELL = [
